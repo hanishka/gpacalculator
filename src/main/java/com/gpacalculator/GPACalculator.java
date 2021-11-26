daf1f12136fddbb278d1045feb18e2f219e58d6d
Hanishka Polimera <polimera.hanishka@gmail.com>
2021-11-26 09:51:37 -0600

    first git commit for GPACalculator project

---
+++ src/main/java/com/gpacalculator/GPACalculator.java
@@ -0,0 +1,11 @@
+package com.gpacalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPACalculator {
+    public static void main(String[] args) {
+        SpringApplication.run(GPACalculator.class, args);
+    }
+}
